df35e3cc44d2de8af8b67bb81250e35c0596dbd3
Dherisma Hanindita <dherismadedek@gmail.com>
2021-03-31 09:43:44 +0700

    Pertemuan7

---
+++ Pertemuan_7/Codingan/Tiket.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dherisma 
+ * Nama    : Dherisma Hanindita Utami
+ * NIM/ No : 2041720018/09
+ * Kelas   : TI 1H
+ */
+public class Tiket {
+    String maskapai, asal, tujuan;
+    int harga;
+    
+    Tiket(String m, int h, String a, String t){
+        maskapai = m;
+        harga = h;
+        asal = a;
+        tujuan = t;
+    }
+}
