37ca2ce6bc7599100f754f621552f0183fd7f3e7
Dherisma Hanindita <dherismadedek@gmail.com>
2021-04-14 18:38:36 +0700

    Pertemuan9

---
+++ Pertemuan_9/Codingan/Praktikum1/Stack.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum1;
+
+/**
+ *
+ * @author Dherisma 
+ * Nama    : Dherisma Hanindita Utami
+ * NIM/ No : 2041720018/09
+ * Kelas   : TI 1H
+ */
+public class Stack {
+    int size;
+    int top;
+    int data[];
+    
+    Stack(int size){
+        this.size = size;
+        data = new int[size];
+        top = -1;
+    }
+    
+    boolean isEmpty(){
+        if(top == -1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    boolean isFull(){
+        if(top == size -1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    void push(int dt){
+        if(!isFull()){
+            top++;
+            data[top] = dt;
+        }else{
+            System.out.println("STACK PENUH!!");
+        }
+    }
+    
+     void pop(){
+        if(!isEmpty()){
+            int x = data[top];
+            top--;
+            System.out.println("Data yang keluar : " + x);
+        }else{
+            System.out.println("Isi stack masih kosong");
+        }
+    }
+     
+    void peek(){
+        System.out.println("Elemen teratas : " + data[top]);
+    }
+    
+    void print(){
+        System.out.println("Isi stack : ");
+        for(int i=top;i>=0;i--){
+            System.out.println(data[i] + " ");
+        }
+        System.out.println("");
+    }
+    
+    void clear(){
+        if(!isEmpty()){
+            for(int i=top;i>=0;i--){
+                top--;
+            }
+            System.out.println("Stack sudah dikosongkan");
+        }else{
+            System.out.println("GAGAL!, Stack masih kosong!");
+        }
+    }
+}
---
+++ Pertemuan_9/Laporan/Jawaban.Laporan.md
@@ -0,0 +1,65 @@
+# JOBSHEET VII (Pertemuan 9)
+# STACK
+### Dherisma Hanindita Utami
+### 2041720018/ 09/ TI 1H
+
+## 7.2
+### 7.2.3 Pertanyaan
+<img src="./ss/1.jpeg">
+
+Jawaban
+1. Fungsi angka 5 pada potongan kode program diatas adalah untuk menginisialisai size array pada class STACK()
+2. Hasil
+<img src="./ss/2.jpeg">
+
+3. Data yang dimasukkan hanya 18 dan angka 40 tidak dimasukkan karena
+data stack sudah memenuhi tempat dan sudah penuh
+
+## 7.3
+### 7.3.3 Pertanyaan
+<img src="./ss/3.jpeg">
+
+Jawaban
+1. Data yang tersimpan pada variabel bk adalah judul, nama , tahun, harga dan jumlah
+2. 
+3. Fungsi penggunaan do-while yang berada pada class StackMain adalah untul meloopinh isi data dari buku, jika ingin menambahkan tekan y untuk Ya atau n jika tidak
+
+## 7.4
+### 7.4.3 Pertanyaan
+<img src="./ss/3.jpeg">
+
+Jawaban :
+1. Alur kerja pada derajat yaitu :
+
+        int derajat(char c){
+            switch(c){
+                case '^':
+                return 3;
+                
+                case '%':
+                return 2;
+                
+                case '*':
+                return 2;
+                
+                case '*':
+                return 2;
+                
+                case '-':
+                return 1;
+                
+                case '+':
+                return 1;
+                default:
+                return 0;
+            }
+        }
+
+2. Fungsi kode 
+
+        c = Q.chartArt(i);
+
+    adalah dimana setiap karakter akan di looping dan dicek di char bagian ke berapa, dan akan memeriksa setiap karakter/huruf
+3. 
+4. Tanda kurang tidak ditampilkan pada hasil konversi karena untuk memeriksan dan memastikan semua karakter yang masih berada si stack setelah semua persamaan terbaca, dan akan dipindahkan ke postfix
+
