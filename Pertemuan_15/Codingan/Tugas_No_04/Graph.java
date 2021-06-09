fcbb3da30c981e22009879f60445d5217ae222be
Dherisma Hanindita <dherismadedek@gmail.com>
2021-06-09 21:44:48 +0700

    Pertemuan_15

---
+++ Pertemuan_15/Codingan/Tugas_No_04/Graph.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas_No_04;
+
+/**
+ *
+ * @author Dherisma 
+ * Nama    : Dherisma Hanindita Utami
+ * NIM/ No : 2041720018/09
+ * Kelas   : TI 1H
+ */
+import java.util.Scanner;
+import java.util.LinkedList;
+public class Graph {
+    int vertex;
+    int type;
+    String[] saveInd;
+
+}
